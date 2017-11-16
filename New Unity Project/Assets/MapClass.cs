a8911a7686e92d5d5e1051fbf9051935cc31fc13
tb1009 <tb1009@york.ac.uk>
2017-11-16 10:57:52 +0000

    Intial Map & Sector Classes

---
+++ New Unity Project/Assets/MapClass.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapClass : MonoBehaviour {
+
+	private List<List<GameObject>> sectorGraph = new List<List<GameObject>>();
+
+	// Use this for initialization
+	void Start () {
+		foreach (Transform child in transform){
+			if (child.name.Substring (0, 6) == "Sector") {
+				List<GameObject> currentSector = new List<GameObject> ();
+				currentSector.Add (child.gameObject);
+				SectorClass sect_cls = child.GetComponent<SectorClass> ();
+				List<GameObject> adjacent_sectors = sect_cls.get_adjacent_sectors ();
+
+				currentSector.AddRange (adjacent_sectors);
+				this.sectorGraph.Add (currentSector);
+			}
+		}
+
+		this.print_sectorGraph ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void print_sectorGraph(){
+		//Printing sectors (for debug);
+		foreach (List<GameObject> x in this.sectorGraph) {
+			string output = "";
+			foreach (GameObject y in x) {
+				output += y.name + " ";
+			}
+			print (output);
+		}
+	}
+}
---
+++ New Unity Project/Assets/SectorClass.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SectorClass : MonoBehaviour {
+	private int attack_value;
+	private int defence_value;
+
+	public List<int> adjacentSectorIDs = new List<int>();
+	private List<GameObject> adjacent_sectors = new List<GameObject> ();
+
+	public bool is_college;
+
+	// Use this for initialization
+	void Start () {
+		if (adjacentSectorIDs.Count == 0) {
+			throw new System.Exception("In '/Map/"+name+"': A sector must have at least one adjacent sector.");
+		} else {
+			//Convert SectorIDs into a list of adjacent sectors
+			foreach (int sectorID in adjacentSectorIDs) {
+				GameObject sect = GameObject.Find ("/Map/Sector #" + sectorID);
+				if (sect != null) {				//Finds the sector and checks if it exists
+					if (sect.name != name) {
+						if (adjacent_sectors.Contains (sect) == false) {
+							adjacent_sectors.Add (sect);	//adjacent sector added to adjacent_sectors list
+						} else {
+							throw new System.Exception ("In '/Map/"+name+"':AdjacentSector list cannot contain duplicates.");
+						}
+					} else {
+						throw new System.Exception ("In '/Map/"+name+"': A sector cannot be adjacent to itself.");
+					}
+				} else {
+					throw new System.Exception ("In '/Map/"+name+"': /Map/Sector #" + sectorID + " cannot be found.");
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public int get_attack_value(){
+		return this.attack_value;
+	}
+
+	public int get_defence_value(){
+		return this.defence_value;
+	}
+
+	public List<GameObject> get_adjacent_sectors(){
+		return this.adjacent_sectors;
+	}
+}
